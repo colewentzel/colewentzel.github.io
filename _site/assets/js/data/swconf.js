ae5fb34b50934161e90632d8767d35d287c73685
Cole Wentzel <85313355+colewentzel@users.noreply.github.com>
2024-04-26 02:03:58 -0400

    Writing progress.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714082282',resources: [
+    cacheName: 'chirpy-1714111010',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
